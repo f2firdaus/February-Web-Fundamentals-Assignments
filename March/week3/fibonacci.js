e3c947750bb1069b72b086de4bf43befb2da758d
f2firdaus <47087219+f2firdaus@users.noreply.github.com>
2022-03-20 12:46:48 +0530

    Update fibonacci.js

--- March/week3/fibonacci.js
+++ March/week3/fibonacci.js
@@ -1,1 +1,11 @@
-
+function fibonacci(num){
+    let fib=[0,1];
+    let data=[]
+    for(let i=2;i<=num;i++){
+        fib[i]=fib[i-1]+fib[i-2];
+        data.push(fib[i]);
+    }
+    return data;
+}
+let number=fibonacci(10);
+console.log(number)
